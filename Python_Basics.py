dbd112de19849a75f99a6f07649772f34f349cb3
Damongregg10 <damongregg10@hotmail.com>
2024-05-17 12:10:50 +1000

    Add files via upload

--- Python_Basics.py
+++ Python_Basics.py
@@ -1,9 +1,13 @@
 import random
 
-Animals = ["Koala", "Tiger", "Dog", "Platypus", "Sloth"]
-Food = ["Eggs", "Beef", "Protien Powder", "Bacon", "Chicken"]
-Verbs = ["Eats", "Makes"]
+Animals = ["Koalas", "Tigers", "Dogs", "Platypuses", "Sloths"]
+Food = ["Eggs", "Beef", "Sausages", "Bacon", "Chicken"]
+Verbs = ["eating", "making"]
+Active_Verbs = ["dancing", "running", "fighting", "cooking"]
+Emotion = ["love", "enjoy", "prefer"]
+Conjunction = ["and", "also they"]
+com = ","
 #print(type(Animals))
 Animals.append ("Elephant")
 #print(Animals[1], Verbs[1], Food[3])
-print(random.choice(Animals), random.choice(Verbs), random.choice(Food))
+print(random.choice(Animals), random.choice(Emotion), random.choice(Verbs), random.choice(Food),com, random.choice(Conjunction), random.choice(Emotion), random.choice(Active_Verbs))
